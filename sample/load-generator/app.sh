aa3c50d687d97b04042b35331d310f14bc10d201
Johannes Unterstein <junterstein@mesosphere.io>
2017-07-07 11:42:32 +0200

    shell version

---
+++ sample/load-generator/app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo $VIP_APP
+for ((i=0; i<=AMOUNT; i++));
+do
+	echo "Run: $i"
+	k="K$i"
+	v=`pwgen 20`
+	data="{\"key\": \"$k\",\"value\":\"$v\"}"
+	echo $data
+	curl -X POST -H 'Content-Type: application/json' $VIP_APP -d "$data"
+	sleep 1
+done
